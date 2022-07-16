01849425527e7496e91518b03e069953f9c98e99
Aaron Su <aaronsu11@outlook.com>
2022-07-17 03:57:17 +1000

    API to create ground plane

--- exts/rv.services.isaac/rv/services/isaac/extension.py
+++ exts/rv.services.isaac/rv/services/isaac/extension.py
@@ -1,11 +1,40 @@
 import carb
+import asyncio
 import omni.ext
 import omni.ui as ui
 from omni.services.core import main
+from omni.isaac.core import World
+from omni.isaac.core.scenes.scene import Scene
+
+from rv.services.isaac.base_sample import BaseSample
+
 
 def ping():
     return "pong"
-    
+
+
+class HelloWorld(BaseSample):
+    def __init__(self) -> None:
+        super().__init__()
+        return
+
+    def setup_scene(self, scene: Scene):
+        scene.add_default_ground_plane()
+        return
+
+    async def setup_post_load(self):
+        return
+
+    async def setup_pre_reset(self):
+        return
+
+    async def setup_post_reset(self):
+        return
+
+    def world_cleanup(self):
+        return
+
+
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() is called.
@@ -17,7 +46,9 @@ class IsaacServiceExtension(omni.ext.IExt):
         frontend_port = carb.settings.get_settings().get_as_int("exts/omni.services.transport.server.http/port")
         print(f"[rv.services.isaac] Starting Isaac Sim Microservice at port {frontend_port}")
 
-        main.register_endpoint("get", "/ping", ping)
+        self._sample = HelloWorld()
+
+        main.register_endpoint("get", "/create_world_async", self._sample.load_world_async)
 
         self._window = ui.Window("My Window", width=300, height=300)
         with self._window.frame:
@@ -26,8 +57,19 @@ class IsaacServiceExtension(omni.ext.IExt):
 
                 def on_click():
                     print("clicked!")
+                    self._on_load_world()
 
                 ui.Button("Click Me", clicked_fn=lambda: on_click())
+
+
+    def _on_load_world(self):
+        async def _on_load_world_async():
+            await self._sample.load_world_async()
+            await omni.kit.app.get_app().next_update_async()
+
+        asyncio.ensure_future(_on_load_world_async())
+        return
+
 
     def on_shutdown(self):
         main.deregister_endpoint("get", "/ping")
